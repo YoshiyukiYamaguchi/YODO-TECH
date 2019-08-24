db270193a1850f0e8b1b3587161db3cd7ec2a48a
yocchi <yysuperjt@gmail.com>
2019-08-24 15:28:48 +0900

    @yocchi1_2 Practice1_1ADD

--- app/source/yy-tech-basic/src/main/java/pr_1/A.java
+++
@@ -1,7 +0,0 @@
-package pr_1;
-
-public class A {
-	public static void main(String []args) {
-		System.out.println("Hello World");
-	}
-}
---
+++ app/source/yy-tech-basic/src/main/java/pr_1/Practice1_1.java
@@ -0,0 +1,5 @@
+package pr_1;
+
+public class Practice1_1 {
+	
+}
