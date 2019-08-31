a54b4066173e70ba48260f5313878674a197a3cf
yocchi <yysuperjt@gmail.com>
2019-08-31 14:20:01 +0900

    @yocchi 【RETURN】1_6 問題文ちゃんと見なさい

--- app/source/yy-tech-basic/src/main/java/pr_1/Pr1_6.java
+++ app/source/yy-tech-basic/src/main/java/pr_1/Pr1_6.java
@@ -1,7 +1,8 @@
 package pr_1;
 
 public class Pr1_6 {
-	public static void main(String[]argus) {
+	public static void main(String[] argus) {
+//		問題文を見直しなさい
 		int x = 7;
 		System.out.println(x * 3 / 2);
 	}
